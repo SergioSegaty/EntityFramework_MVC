c6085beff956f2035542ae32982f20d71704cddd
Sergio Segaty <sergio.segaty@gmail.com>
2019-07-10 14:13:18 -0300

    Desenvolvimento Entity + WEB(MVC)
    Instalação de Packages.
    
    Criação dos Modelos principais.

---
+++ View/Models/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace View.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
